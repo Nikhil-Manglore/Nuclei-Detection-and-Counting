2fdf3accbae99be07781c7f32411e99fa55ab94f
Nikhil Manglore <46588759+Nikhil-Manglore@users.noreply.github.com>
2021-12-21 15:26:49 -0800

    Update __init__.py

--- src/NDaC_image/watershed/__init__.py
+++ src/NDaC_image/watershed/__init__.py
@@ -1,14 +1,9 @@
 ################################################################################
-# File collaboration:                                                          #
-#   Tikhon Pachin                                                              #
-################################################################################
 # Grayscale image shape: (num_rows, num_pixels_per_row)                        #
 # Grayscale image:                                                             #
 #   numpy.array([ [0, 1, 2...], [intensities], [intensities] ])                #
 ################################################################################
-# Added: x/x/2021 - First Last
 from .watershedCV import *
 
-# Added: 11/2/2021 - Tikhon Pachin
 from .watershed import *
 
