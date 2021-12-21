d60dc3458fca0c8d27bc3ed280138e47106f3da1
Nikhil Manglore <46588759+Nikhil-Manglore@users.noreply.github.com>
2021-12-21 15:20:46 -0800

    Update __init__.py

--- src/NDaC_image/distance_transform/__init__.py
+++ src/NDaC_image/distance_transform/__init__.py
@@ -1,12 +1,9 @@
 ################################################################################
-# File collaboration:                                                          #
-#   Tikhon Pachin                                                              #
-################################################################################
 # Grayscale image shape: (num_rows, num_pixels_per_row)                        #
 # Grayscale image:                                                             #
 #   numpy.array([ [0, 1, 2...], [intensities], [intensities] ])                #
 ################################################################################
-# Added: 10/18/2021 - Tikhon Pachin
+# Added: 10/18/2021
 from .chessboard_distance_transform import *
 
 from .euclidean_distance_transform import *
