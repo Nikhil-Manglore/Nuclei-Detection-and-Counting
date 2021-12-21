1ac2c4053f569e70a2969052e7cddfb22a35dcc1
Nikhil Manglore <46588759+Nikhil-Manglore@users.noreply.github.com>
2021-12-21 15:23:48 -0800

    Update __init__.py

--- src/NDaC_image/filters/__init__.py
+++ src/NDaC_image/filters/__init__.py
@@ -1,27 +1,24 @@
 ################################################################################
-# File collaboration:                                                          #
-#   Tikhon Pachin                                                              #
-################################################################################
 # Grayscale image shape: (num_rows, num_pixels_per_row)                        #
 # Grayscale image:                                                             #
 #   numpy.array([ [0, 1, 2...], [intensities], [intensities] ])                #
 ################################################################################
-# Added: 9/14/2021 - Tikhon Pachin
+# Added: 9/14/2021
 from .erosion import *
 from .dilation import *
 
-# Added: 9/28/2021 - Tikhon Pachin
+# Added: 9/28/2021
 from .edge_detection import *
 from .opening import *
 from .closing import *
 
-# Added: 10/4/2021 - Tikhon Pachin
+# Added: 10/4/2021
 from .gaussian import *
 
-# Added: 11/07/2021 - Yu-Hsuan Lin
+# Added: 11/07/2021
 from .median_filter import *
 from .average_filter import *
 
-# Added: 11/23/2021 - Yu-Hsuan Lin
+# Added: 11/23/2021
 from .ceiling import *
 from .conditional_erosion import *
