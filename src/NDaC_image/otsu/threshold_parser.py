625567222a7441afcaf26afb420395b852e9e5ae
Nikhil Manglore <46588759+Nikhil-Manglore@users.noreply.github.com>
2021-12-21 15:30:08 -0800

    Update threshold_parser.py

--- src/NDaC_image/otsu/threshold_parser.py
+++ src/NDaC_image/otsu/threshold_parser.py
@@ -1,10 +1,5 @@
 ################################################################################
-# File collaboration:                                                          #
-#   Nikhil Manglore - 9/11/2021                                                #
-#   Tikhon Pachin - 10/12/2021                                                 #
-################################################################################
 # Import necessary tools for further operations
-# Tikhon Pachin - 10/12/2021
 import numpy as np
 
 # Parameters:
@@ -13,8 +8,6 @@ import numpy as np
 # Return Value - The array with all the values of the pixels converted to either 0 or 255.
 # This function will loop over all the pixel values and will set them to 0 or 255 depending on whether or not they are greater than the threshold value.
 
-# Update: switched from loops to vectors.
-# Tikgon Pachin - 10/12/2021
 def threshold_parser(image, threshold):
     np.place(image, image <= threshold, 0)
     np.place(image, image > threshold, 255)
