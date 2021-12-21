ea5a40b1fdd52eed9f8030f7b82670dfefb62537
Nikhil Manglore <46588759+Nikhil-Manglore@users.noreply.github.com>
2021-12-21 15:25:35 -0800

    Update interest_area_detection.py

--- src/NDaC_image/sequences/interest_area_detection.py
+++ src/NDaC_image/sequences/interest_area_detection.py
@@ -1,7 +1,4 @@
 ################################################################################
-# File collaboration:                                                          #
-#   Tikhon Pachin                                                              #
-################################################################################
 # Colored image shape: (num_rows, num_pixels_per_row, RGB == const 3)          #
 # Grayscale image shape: (num_rows, num_pixels_per_row)                        #
 # Colored image:                                                               #
@@ -11,7 +8,6 @@
 ################################################################################
 
 # Importing necessary tools for the further operations.
-# Tikhon Pachin - 10/16/2021
 import numpy as np
 
 # Area of interest detector. Tikhon Pachin 10/16/2021
@@ -22,7 +18,6 @@ def detect_area_of_interest(img, n_blur, n_dilation, v_exposure):
     blurred_image = img.get_gaussian_blur_image(np.copy(img.gray_img), n_blur)
 
     # Threshold the image after the noise removal.
-    # Tikhon Pachin - 10/16/2021
     blur_otsu_threshold = img.get_otsu_threshold(np.copy(blurred_image))
     blur_otsu_img = img.otsu_parser(np.copy(blurred_image), blur_otsu_threshold)
 
@@ -33,13 +28,11 @@ def detect_area_of_interest(img, n_blur, n_dilation, v_exposure):
 
     # Increase the intensity of each pixel in the areas of interest, so that
     # Otsu's thresholding does pick up on the dark foregrounds.
-    # Tikhon Pachin - 10/16/2021
     exposure_mask = np.copy(dilated_image)
     np.place(exposure_mask, exposure_mask == 255, v_exposure)
     masked_img = np.copy(img.gray_img) + exposure_mask
 
     # Threshold the image to get the areas of interest.
-    # Tikhon Pachin - 10/16/2021
     exp_otsu_threshold = img.get_otsu_threshold(np.copy(masked_img))
     area_of_interest = img.otsu_parser(np.copy(masked_img), exp_otsu_threshold)
     
