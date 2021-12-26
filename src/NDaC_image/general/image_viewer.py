1e8a1a6566bb4ef537e3b2b5a59ef44321356fbf
Nikhil Manglore <46588759+Nikhil-Manglore@users.noreply.github.com>
2021-12-25 23:03:41 -0800

    Update image_viewer.py

--- src/NDaC_image/general/image_viewer.py
+++ src/NDaC_image/general/image_viewer.py
@@ -1,4 +1,4 @@
-## Updated 09/21/2021 - Yu-Hsuan Lin
+## Added 09/21/2021
 import numpy as np
 from PIL import Image 
 
@@ -6,7 +6,6 @@ from PIL import Image
 ## img: A list or a numpy array with shape greater than (1,1). 
 
 # Switched imageViewer to image_viewer - standard nomenclature.
-# Tikhon Pachin - 10/12/2021
 def image_viewer(img):
     if (np.shape(img)[0] == 0) or (np.shape(img)[1] == 0):
         raise ValueError("Shape of array has to be greater or equal to (1,1)")
