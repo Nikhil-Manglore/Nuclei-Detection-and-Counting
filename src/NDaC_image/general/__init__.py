9d80c4ca9ec3a94e04510e8cc2c605edabb857bc
Nikhil Manglore <46588759+Nikhil-Manglore@users.noreply.github.com>
2021-12-25 23:03:06 -0800

    Update __init__.py

--- src/NDaC_image/general/__init__.py
+++ src/NDaC_image/general/__init__.py
@@ -1,7 +1,4 @@
 ################################################################################
-# File collaboration:                                                          #
-#   Tikhon Pachin                                                              #
-################################################################################
 # Colored image shape: (num_rows, num_pixels_per_row, RGB == const 3)          #
 # Grayscale image shape: (num_rows, num_pixels_per_row)                        #
 # Colored image:                                                               #
@@ -9,7 +6,6 @@
 # Grayscale image:                                                             #
 #   numpy.array([ [0, 1, 2...], [intensities], [intensities] ])                #
 ################################################################################
-# Added: 9/20/2021 - Tikhon Pachin
 from .image_viewer import *
 from .grayscale_conversion import *
 from .image_uploader import *
