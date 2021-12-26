cd6f9a44805e5e4f24fe95cbffbc37c0b6bb85f8
Nikhil Manglore <46588759+Nikhil-Manglore@users.noreply.github.com>
2021-12-25 23:03:49 -0800

    Update image_viewer.py

--- src/NDaC_image/general/image_viewer.py
+++ src/NDaC_image/general/image_viewer.py
@@ -5,7 +5,6 @@ from PIL import Image
 ## imageViewer: Function that takes in an array and show it as a grayscale image.
 ## img: A list or a numpy array with shape greater than (1,1). 
 
-# Switched imageViewer to image_viewer - standard nomenclature.
 def image_viewer(img):
     if (np.shape(img)[0] == 0) or (np.shape(img)[1] == 0):
         raise ValueError("Shape of array has to be greater or equal to (1,1)")
